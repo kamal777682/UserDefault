f5a57048a8c2fa7bd0b72e9f7496eda0e8053186
kamalpreet <kamal777682>
2020-03-04 21:20:23 -0500

    user default

--- UserDefault/ViewController.swift
+++ UserDefault/ViewController.swift
@@ -10,11 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet var txtName: UITextField!
+    @IBOutlet var txtPassword: UITextField!
+    @IBOutlet var lblName: UILabel!
+    @IBOutlet var btnSwitch: UISwitch!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+   
+    override func viewWillAppear(_ animated: Bool) {
+        txtName.text = ""
+        lblName.text = ""
+    }
+   
+    @IBAction func btnSaveClick(_ sender: UIButton)
+    {
+        if txtName.text == txtPassword.text && btnSwitch.isOn
+        {
+            UserDefaults.standard.set(txtName.text, forKey: "name")
+            UserDefaults.standard.set(txtPassword.text,forKey: "name")
+        }
+            
+    }
+    
+    @IBAction func btnFetchClick(_ sender: UIButton)
+    {
+        let ud = UserDefaults.standard
+        let name = ud.string(forKey: "name")
+        lblName.text = name
+    }
+    
+    @IBAction func btnNextClick(_ sender: UIButton)
+    {
+    }
 }
 
